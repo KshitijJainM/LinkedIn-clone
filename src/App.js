c9ee9e5ad657518b339cd1023e85f8bd97929d63
KshitijJainM <2018ise_kshitijjainm@nie.ac.in>
2021-04-11 13:10:44 +0530

    Add Feed and Link Firebase

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import "./App.css";
 import SideBar from "./SideBar";
+import Feed from "./Feed";
 function App() {
 	return (
 		<div className="app">
 			<Header />
 			<div className="app__body">
 				<SideBar />
+				<Feed />
 			</div>
 		</div>
 	);
